a31bb0b2dbd141b1700d008466913dc1e3f0b774
Jean Barbosa <jeanbarbosa9@gmail.com>
2020-08-27 10:18:49 -0300

    Componente de exemplo com estilização SC

--- client/src/components/Main/index.tsx
+++ client/src/components/Main/index.tsx
@@ -1,7 +1,20 @@
+import * as S from './styles'
+
 const Main = () => (
-  <main>
-    <h1>React Avançado</h1>
-  </main>
+  <S.Wrapper>
+    <S.Logo
+      src="/img/logo.svg"
+      alt="Image de um átomo e React Avançado ao lado."
+    />
+    <S.Title>React Avançado - Boilerplate</S.Title>
+    <S.Description>
+      TypeScript, ReactJS, NextJS e Styeld Components!
+    </S.Description>
+    <S.Illustration
+      src="/img/hero-illustration.svg"
+      alt="Pessoa desenvolvendo código de programação no computador"
+    />
+  </S.Wrapper>
 )
 
 export default Main
