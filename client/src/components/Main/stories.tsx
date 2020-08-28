5f3a5427fb3c6f38de367471e9af62d631a43bd0
Jean Barbosa <jeanbarbosa9@gmail.com>
2020-08-28 17:52:59 -0300

    Utilizando args e argTypes para setar argumentos padrões no story

--- client/src/components/Main/stories.tsx
+++ client/src/components/Main/stories.tsx
@@ -1,20 +1,26 @@
 import { Story, Meta } from '@storybook/react/types-6-0'
-import { withKnobs, text } from '@storybook/addon-knobs'
 
 import Main from '.'
 
 export default {
   title: 'Main',
   component: Main,
-  decorators: [withKnobs]
+  argTypes: {
+    background: {
+      control: 'color'
+    }
+  },
+  args: {
+    title: 'Title Default',
+    description: 'Description Default'
+  }
 } as Meta
 
-export const Basic: Story = () => (
-  <Main
-    title={text('Title', 'React Avançado - Boilerplate')}
-    description={text(
-      'Description',
-      'TypeScript, ReactJS, NextJS e Styeld Components!'
-    )}
-  />
-)
+export const Basic: Story = (args) => <Main {...args} />
+Basic.args = {
+  title: "Basic's title",
+  description: "Description's title",
+  background: '#012787'
+}
+
+export const Default: Story = (args) => <Main {...args} />
