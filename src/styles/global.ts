caa3df2c4d02f72e90e599a13b879f5d7c46d837
Jean Barbosa <jeanbarbosa9@gmail.com>
2020-09-30 12:20:17 -0300

    Implements ThemeProvider

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,4 +1,4 @@
-import { createGlobalStyle } from 'styled-components'
+import { createGlobalStyle, css } from 'styled-components'
 
 const GlobalStyle = createGlobalStyle`
   * {
@@ -11,12 +11,16 @@ const GlobalStyle = createGlobalStyle`
     height: 100%;
   }
 
-  html {
-    font-size: 62.5%
-  }
+  ${({ theme }) => css`
+    html {
+      font-size: 62.5%;
+    }
 
-  body {
-    font-family: --apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-  }
+    body {
+      font-family: ${theme.font.family};
+      font-size: ${theme.font.sizes.medium};
+    }
+  `}
+
 `
 export default GlobalStyle
